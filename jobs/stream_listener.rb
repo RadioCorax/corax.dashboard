dcfdc379c31ae3c5ced9b3075d531a887f0f0686
Stefan Walluhn <stefan@neuland.io>
2025-12-27 23:34:13 +0100

    add timeout when requesting data from icecast

--- jobs/stream_listener.rb
+++ jobs/stream_listener.rb
@@ -5,7 +5,7 @@ require 'faraday'
 points = []
 
 SCHEDULER.every '1m' do
-  conn = Faraday.new(settings.icecast_url) do |builder|
+  conn = Faraday.new(settings.icecast_url, requests: { timeout: 15 }) do |builder|
     builder.response(:json)
     builder.response(:raise_error)
   end
